8539e86e805066bb07993b174b4eae94969c3a91
Omar <omarashraffci@gmail.com>
2023-11-22 13:06:24 +0200

    text and color styles first screen

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color buttonsColors = Color(0xFF34A853);
+  static Color primaryTextColor = Colors.black;
+  static Color primaryBackground = Colors.white;
+}
---
+++ lib/core/utils/app_text_styles.dart
@@ -0,0 +1,40 @@
+import 'package:checkout_feature/core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static TextStyle style25 = TextStyle(
+    color: AppColors.primaryTextColor,
+    fontSize: 25,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+  static TextStyle style18 = TextStyle(
+    color: AppColors.primaryTextColor,
+    fontSize: 18,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+  static TextStyle style24 = TextStyle(
+    color: AppColors.primaryTextColor,
+    fontSize: 24,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+  static TextStyle style22 = TextStyle(
+    color: AppColors.primaryTextColor,
+    fontSize: 22,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+  static TextStyle style20 = TextStyle(
+    color: AppColors.primaryTextColor,
+    fontSize: 20,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+}
