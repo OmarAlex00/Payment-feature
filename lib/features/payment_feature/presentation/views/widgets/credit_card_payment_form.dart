f12821b554f5d1936642b16075d3b85b7e1de67b
Omar <omarashraffci@gmail.com>
2023-11-27 16:17:24 +0200

    form UI done

---
+++ lib/features/payment_feature/presentation/views/widgets/credit_card_payment_form.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+
+class CreditCardPaymentForm extends StatefulWidget {
+  const CreditCardPaymentForm(
+      {super.key, required this.autovalidateMode, required this.formKey});
+  final GlobalKey<FormState> formKey;
+  final AutovalidateMode autovalidateMode;
+  @override
+  State<CreditCardPaymentForm> createState() => _CreditCardPaymentFormState();
+}
+
+class _CreditCardPaymentFormState extends State<CreditCardPaymentForm> {
+  String cardNumber = '', expiryDate = '', cardHolderName = '', cvvCode = '';
+  bool showBack = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CreditCardWidget(
+          isHolderNameVisible: true,
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          showBackView: showBack,
+          onCreditCardWidgetChange: (creditCardBrand) {},
+        ),
+        CreditCardForm(
+            autovalidateMode: widget.autovalidateMode,
+            cardNumber: cardNumber,
+            expiryDate: expiryDate,
+            cardHolderName: cardHolderName,
+            cvvCode: cvvCode,
+            onCreditCardModelChange: (value) {
+              cardHolderName = value.cardHolderName;
+              cardNumber = value.cardNumber;
+              expiryDate = value.expiryDate;
+              cvvCode = value.cvvCode;
+              showBack = value.isCvvFocused;
+              setState(() {});
+            },
+            formKey: widget.formKey)
+      ],
+    );
+  }
+}
--- lib/features/payment_feature/presentation/views/widgets/payment_details_body.dart
+++ lib/features/payment_feature/presentation/views/widgets/payment_details_body.dart
@@ -1,63 +1,47 @@
+import 'dart:developer';
+
+import 'package:checkout_feature/core/utils/app_strings.dart';
+import 'package:checkout_feature/core/widgets/custom_button.dart';
+import 'package:checkout_feature/features/payment_feature/presentation/views/widgets/credit_card_payment_form.dart';
 import 'package:checkout_feature/features/payment_feature/presentation/views/widgets/payment_gateways_list_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_credit_card/flutter_credit_card.dart';
 
-class PaymentDetailsBody extends StatelessWidget {
+class PaymentDetailsBody extends StatefulWidget {
   const PaymentDetailsBody({
     super.key,
   });
 
   @override
+  State<PaymentDetailsBody> createState() => _PaymentDetailsBodyState();
+}
+
+class _PaymentDetailsBodyState extends State<PaymentDetailsBody> {
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  final GlobalKey<FormState> formKey = GlobalKey();
+
+  @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
-        children: [const PaymentGateWaysListView(), CreditCardPaymentForm()],
-      ),
-    );
-  }
-}
-
-class CreditCardPaymentForm extends StatefulWidget {
-  final GlobalKey<FormState> formKey = GlobalKey();
-
-  CreditCardPaymentForm({super.key});
-  @override
-  State<CreditCardPaymentForm> createState() => _CreditCardPaymentFormState();
-}
-
-class _CreditCardPaymentFormState extends State<CreditCardPaymentForm> {
-  String cardNumber = '', expiryDate = '', cardHolderName = '', cvvCode = '';
-  bool showBack = false;
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CreditCardWidget(
-          isHolderNameVisible: true,
-          cardNumber: cardNumber,
-          expiryDate: expiryDate,
-          cardHolderName: cardHolderName,
-          cvvCode: cvvCode,
-          showBackView: showBack,
-          onCreditCardWidgetChange: (creditCardBrand) {},
-        ),
-        CreditCardForm(
+        children: [
+          const PaymentGateWaysListView(),
+          CreditCardPaymentForm(
             autovalidateMode: autovalidateMode,
-            cardNumber: cardNumber,
-            expiryDate: expiryDate,
-            cardHolderName: cardHolderName,
-            cvvCode: cvvCode,
-            onCreditCardModelChange: (value) {
-              cardHolderName = value.cardHolderName;
-              cardNumber = value.cardNumber;
-              expiryDate = value.expiryDate;
-              cvvCode = value.cvvCode;
-              showBack = value.isCvvFocused;
-              setState(() {});
+            formKey: formKey,
+          ),
+          CustomButton(
+            title: AppStrings.pay,
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+                log('Success');
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+              }
             },
-            formKey: widget.formKey)
-      ],
+          )
+        ],
+      ),
     );
   }
 }
