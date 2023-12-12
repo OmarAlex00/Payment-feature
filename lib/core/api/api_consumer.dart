32f4278d9e823113145afec90b5647311ce6800e
Omar <omarashraffci@gmail.com>
2023-12-12 16:37:01 +0200

    api service done

--- lib/core/api/api_consumer.dart
+++ lib/core/api/api_consumer.dart
@@ -1,8 +1,7 @@
 import 'package:dio/dio.dart';
 
 abstract class BaseApiConsumer {
-  Future<Response> get(String path,
-      {String? token, required String contentType});
+  Future<Response> get(String path, {String? token, String? contentType});
 
   Future<Response> post(String path,
       {Map<String, dynamic>? body,
@@ -10,46 +9,46 @@ abstract class BaseApiConsumer {
       bool attachment = false,
       String? attachmentKey,
       String? attachmentPath,
-      required String contentType});
+      String? contentType});
 
   Future<Response> put(String path,
-      {Map<String, dynamic>? body, String? token, required String contentType});
+      {Map<String, dynamic>? body, String? token, String? contentType});
 
-  Future<Response> delete(String path,
-      {String? token, required String contentType});
+  Future<Response> delete(String path, {String? token, String? contentType});
 }
 
 class ApiConsumer implements BaseApiConsumer {
   final Dio dio = Dio();
   @override
   Future<Response> get(String path,
-      {String? token, required String contentType}) async {
+      {String? token, String? contentType}) async {
     late Map<String, String> headers;
     if (token != null) {
       headers = {
-        'Content-Type': contentType,
+        'Content-Type': contentType!,
         'Authorization': 'Bearer $token',
       };
     } else {
-      headers = {'Content-Type': contentType};
+      headers = {'Content-Type': contentType!};
     }
-    var response = dio.get(path, options: Options(headers: headers));
+    var response = dio.get(path,
+        options: Options(headers: headers, contentType: contentType));
     return response;
   }
 
   @override
-  Future<Response> delete(String path,
-      {String? token, required String contentType}) {
+  Future<Response> delete(String path, {String? token, String? contentType}) {
     late Map<String, String> headers;
     if (token != null) {
       headers = {
-        'Content-Type': contentType,
+        'Content-Type': contentType!,
         'Authorization': 'Bearer $token',
       };
     } else {
-      headers = {'Content-Type': contentType};
+      headers = {'Content-Type': contentType!};
     }
-    var response = dio.delete(path, options: Options(headers: headers));
+    var response = dio.delete(path,
+        options: Options(headers: headers, contentType: contentType));
     return response;
   }
 
@@ -60,37 +59,37 @@ class ApiConsumer implements BaseApiConsumer {
       bool attachment = false,
       String? attachmentKey,
       String? attachmentPath,
-      required String contentType}) {
+      String? contentType}) {
     late Map<String, String> headers;
     if (token != null) {
       headers = {
-        'Content-Type': contentType,
+        'Content-Type': contentType!,
         'Authorization': 'Bearer $token',
       };
     } else {
-      headers = {'Content-Type': contentType};
+      headers = {'Content-Type': contentType!};
     }
-    var response =
-        dio.post(path, data: body, options: Options(headers: headers));
+    var response = dio.post(path,
+        data: body,
+        options: Options(headers: headers, contentType: contentType));
     return response;
   }
 
   @override
   Future<Response> put(String path,
-      {Map<String, dynamic>? body,
-      String? token,
-      required String contentType}) {
+      {Map<String, dynamic>? body, String? token, String? contentType}) {
     late Map<String, String> headers;
     if (token != null) {
       headers = {
-        'Content-Type': contentType,
+        'Content-Type': contentType!,
         'Authorization': 'Bearer $token',
       };
     } else {
-      headers = {'Content-Type': contentType};
+      headers = {'Content-Type': contentType!};
     }
-    var response =
-        dio.put(path, data: body, options: Options(headers: headers));
+    var response = dio.put(path,
+        data: body,
+        options: Options(headers: headers, contentType: contentType));
     return response;
   }
 }
